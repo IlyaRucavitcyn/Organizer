c471168f161c383a0acf832a16688b8ecf403fba
Stepan Bozhik <bozhik.stepan@gmail.com>
2017-02-06 16:58:20 +0300

    Tile template added

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,10 @@ const path = require('path'),
 module.exports = {
     context: path.join(__dirname, 'src'),
     entry: {
-      main: './main',
-      styles: './css/style'
+      main: [
+        './main',
+        './css/style'
+      ]
     },
     output: {
         path: path.join(__dirname, 'dist'),
